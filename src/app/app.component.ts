14888e551eb9c804b9d35abbe5420fb399bc4b7f
ThusharAjith <thusharajith@gmail.com>
2025-01-20 10:11:09 +0530

    fixed lifecycle issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root', 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-
-
-  ngOnInit(): void {
-      initFlowbite();
-  }
-
-
+export class AppComponent{
 }
